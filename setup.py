a7c0a5e1f24a2facf959421de8a65db49745a406
Akshat-Gupta04 <e22cseu0059@bennett.edu.in>
2025-01-02 10:13:40 +0530

    Initial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical chatBot',
+    version= '0.0.0',
+    author= 'Akshat Gupta',
+    packages= find_packages(),
+    install_requires = []
+
+)
